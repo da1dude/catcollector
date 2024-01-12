e6020040757a046d284ae1927bc55e88d4639c30
Paul K <pkerschieter@gmail.com>
2024-01-12 13:43:22 -0600

    added all cats page

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+cats = [
+    {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
+    {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
+    {'name': 'AngryCat', 'breed': 'calico', 'description': 'gentle and loving', 'age': 4},
+]
+
 # Create your views here.
 def home(request):
 # Include an .html file extension - unlike when rendering EJS templates
@@ -8,3 +14,9 @@ def home(request):
 def about(request):
 # Include an .html file extension - unlike when rendering EJS templates
     return render(request, 'about.html')
+
+def cats_index(request):
+    # We pass data to a template very much like we did in Express!
+    return render(request, 'cats/index.html', {
+        'cats': cats
+    })
