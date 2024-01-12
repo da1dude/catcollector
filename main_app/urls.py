9155870b0143a7c776d014b60ff48d833f06a42c
Paul K <pkerschieter@gmail.com>
2024-01-12 15:06:47 -0600

    added details page

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    #this is our route for the index page of cats
     path('cats/', views.cats_index, name='index'),
+    #route for the detail page of our cats
+    #we need an id as well as a way to refer to the id(a route paramater)
+    path('cats/<int:cat_id>', views.cats_detail, name='detail')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 
-cats = [
-    {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
-    {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
-    {'name': 'AngryCat', 'breed': 'calico', 'description': 'gentle and loving', 'age': 4},
-]
+from .models import Cat
+
+#used to build initial view
+# cats = [
+#     {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
+#     {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
+#     {'name': 'AngryCat', 'breed': 'calico', 'description': 'gentle and loving', 'age': 4},
+# ]
 
 # Create your views here.
 def home(request):
@@ -16,7 +19,18 @@ def about(request):
     return render(request, 'about.html')
 
 def cats_index(request):
+    #collect our objects from the db
+    cats = Cat.objects.all()
+
+    # for cat in cats:
+    #     print(cat)
     # We pass data to a template very much like we did in Express!
     return render(request, 'cats/index.html', {
         'cats': cats
     })
+
+#detail view - shows 1 cat at '/cats/:id'
+def cats_detail(request, cat_id):
+    #find one cat wiht its id
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/detail.html', { 'cat': cat })
